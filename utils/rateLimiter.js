166786764e737debd5b56501bbd5b6f0df1fdc48
Oluwayimika <oladipupomichael9@gmail.com>
2024-09-14 13:40:51 +0100

    cleanup

--- utils/rateLimiter.js
+++ utils/rateLimiter.js
@@ -1,3 +1,5 @@
+const rateLimit = require("express-rate-limit");
+
 const globalRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
--- utils/tokenization.js
+++ utils/tokenization.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const { jwtExpiryTime } = require("../constants");
 
-const jwtSecret = process.process.JWT_SECRET;
+const jwtSecret = process.env.JWT_SECRET;
 
 const createToken = (payload) => {
   return jwt.sign(payload, jwtSecret, { expiresIn: jwtExpiryTime });
